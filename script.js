2ea789bb557c10111a145ac2713f11b3414e81c3
Laura Villumsen Taboryski <Laura@Lauras-MacBook-Pro-7.local>
2019-01-07 19:40:14 +0100

    final

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+window.addEventListener("load", sidenVises);
 
+//BURGERMENU
+
+function sidenVises() {
+    console.log("siden vises");
+    // registrer klik på menuknap
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden")
+
+    if (erSkjult == true) {
+        //menuen er skjult - ændr menuknap til lll
+        document.querySelector("#menuknap img").src = "UI/menu.png";
+    } else {
+        //menuen er nu vist - ændr menuknap til X
+        document.querySelector("#menuknap img").src = "UI/cancel2.png";
+    }
+}
+
+//BURGERMENU SLUT
